8ded51d1ec244538c54314d80de2ca0038ea893d
Sakesh-A <a.sakesh@gmail.com>
2025-02-03 21:04:41 -0500

    bp vs donor/rec

--- fmt_dataset_visuals/bp_vs_donor_rec.py
+++ fmt_dataset_visuals/bp_vs_donor_rec.py
@@ -5,25 +5,25 @@ import matplotlib.pyplot as plt
 file_path = "C:\\Users\\asake\\OneDrive\\Desktop\\Homework\\FMT\\FMT_dataset.csv"
 df = pd.read_csv(file_path)
 
-# Replace 'post-FMT' with 'postFMT' and 'pre-FMT' with 'preFMT' to avoid duplication
-df['donor_pre_post'] = df['donor_pre_post'].replace({'post-FMT': 'postFMT', 'pre-FMT': 'preFMT'})
-
 # Define the sequencing depth using the number of bases in gigabases
 df_filtered = df.dropna(subset=["number_bases_gigabases", "donor_pre_post"])
 
-# Grouping data by pre, post, donor
-donor_pre_post_var = df_filtered["donor_pre_post"].unique()
+# Replace 'Pre-FMT' with 'PreFMT' and 'Post-FMT' with 'PostFMT'
+df_filtered['donor_pre_post'] = df_filtered['donor_pre_post'].replace({'Pre-FMT': 'PreFMT', 'Post-FMT': 'PostFMT'})
+
+# Grouping data by disease
+donor_patient_types = df_filtered["donor_pre_post"].unique()
 groups = [df_filtered[df_filtered["donor_pre_post"] == condition]["number_bases_gigabases"] 
-          for condition in donor_pre_post_var]
+          for condition in donor_patient_types]
 
 # Set up for the plot
 plt.figure(figsize=(8, 6))
-plt.boxplot(groups, labels = donor_pre_post_var, patch_artist=True)
+plt.boxplot(groups, tick_labels=donor_patient_types, patch_artist=True)
 
 # Labels and title
-plt.xlabel("Pre-FMT, Post-FMT, Donor")
+plt.xlabel("Donor, PreFMT, PostFMT")
 plt.ylabel("Sequencing Depth (Gbp)")
-plt.title("Sequencing Depth by Donor and Recipient")
+plt.title("Sequencing Depth by Condition (Disease Type)")
 plt.xticks(rotation=45)  # Rotate x-axis labels if needed
 plt.grid(axis="y", linestyle="--", alpha=0.7)
 plt.show()
